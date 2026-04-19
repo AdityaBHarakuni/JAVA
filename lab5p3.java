c87c0497732f376976171ed0749ccab6bea6e2e4
AdityaBHarakuni <adityabharakuni@gmail.com>
2026-04-19 19:53:05 +0530

    Add files via upload

---
+++ lab5p3.java
@@ -0,0 +1,39 @@
+package com.swing.labs;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class p3 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Country Capitals");
+        String[] countries = {"USA", "India", "Vietnam", "Canada", "Denmark", 
+                             "France", "Great Britain", "Japan", "Africa", 
+                             "Greenland", "Singapore"};
+
+        HashMap<String, String> capitals = new HashMap<>();
+        capitals.put("USA", "Washington, D.C.");
+        capitals.put("India", "New Delhi");
+        capitals.put("Vietnam", "Hanoi");
+        capitals.put("Canada", "Ottawa");
+        capitals.put("Denmark", "Copenhagen");
+        capitals.put("France", "Paris");
+        capitals.put("Great Britain", "London");
+        capitals.put("Japan", "Tokyo");
+        capitals.put("Africa", "N/A (Continent)");
+        capitals.put("Greenland", "Nuuk");
+        capitals.put("Singapore", "Singapore");
+
+        JList<String> list = new JList<>(countries);
+        list.addListSelectionListener(e -> {
+            if (!e.getValueIsAdjusting()) {
+                String country = list.getSelectedValue();
+                System.out.println("Capital of " + country + ": " + capitals.get(country));
+            }
+        });
+
+        frame.add(new JScrollPane(list));
+        frame.setSize(300, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
